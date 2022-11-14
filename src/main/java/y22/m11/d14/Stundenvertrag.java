511735e154c295fd73745ef899d8cb37dd72275f
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2022-11-14 13:00:01 +0100

    iPad bad

---
+++ src/main/java/y22/m11/d14/Stundenvertrag.java
@@ -0,0 +1,8 @@
+package y22.m11.d14;
+
+public class Stundenvertrag extends Vertrag {
+    private double stundensatz;
+    private int stundenzahl;
+
+    //...
+}
--- src/main/java/y22/m11/d14/Vertrag.java
+++ src/main/java/y22/m11/d14/Vertrag.java
@@ -1,1 +1,13 @@
-public class Vertrag {}
+package y22.m11.d14;
+
+import java.util.Date;
+
+public abstract class Vertrag {
+    private Date vertragsdatum;
+    
+    public Vertrag(Date vertragsdatum) {
+        this.vertragsdatum = vertragsdatum;
+    }
+
+    public abstract double kostenBerechnen();
+}
