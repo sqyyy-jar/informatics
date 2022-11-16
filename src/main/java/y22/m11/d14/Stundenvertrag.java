867cb61ad7e0bb2a83be898e1cd01a22b3453be3
sqyyy <62748369+sqyyy-jar@users.noreply.github.com>
2022-11-16 12:27:43 +0100

    +

--- src/main/java/y22/m11/d14/Stundenvertrag.java
+++ src/main/java/y22/m11/d14/Stundenvertrag.java
@@ -1,8 +1,21 @@
 package y22.m11.d14;
 
+import java.util.Date;
+
 public class Stundenvertrag extends Vertrag {
     private double stundensatz;
     private int stundenzahl;
 
-    //...
+    public Stundenvertrag(Date vertragsdatum, double stundensatz, int stundenzahl) {
+        this(vertragsdatum);
+        this.stundensatz = stundensatz;
+        this.stundenzahl = stundenzahl;
+    }
+
+    @Override
+    public double kostenBerechnen() {
+        return stundenzahl * stundensatz;
+    }
+
+    // getters and setters
 }
