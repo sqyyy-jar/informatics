b01285af4bdf6362f0a0aef313254897b892e73d
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-11-28 11:44:27 +0100

    Carrent

---
+++ src/main/java/y22/m11/d24/carrent/CarRent.java
@@ -0,0 +1,10 @@
+package y22.m11.d24.carrent;
+
+import informatik.Exercise;
+
+@Exercise("CarRent 24.11.2022")
+public class CarRent {
+    public static void run() {
+
+    }
+}
---
+++ src/main/java/y22/m11/d24/carrent/Pkw.java
@@ -0,0 +1,23 @@
+package y22.m11.d24.carrent;
+
+import java.util.Date;
+
+public class Pkw extends Fahrzeug {
+    private double kofferraumGroesse;
+
+    public Pkw(String kennzeichen, String typ, double kmStand, String treibstoff, Date alter, boolean klimaanlage,
+               boolean sitzheitzung, double verbrauch, String schadstoffBelastung, String versicherung,
+               double grundtarif, boolean verfuegbarkeit, int tueren, double leistung, double kofferraumGroesse) {
+        super(kennzeichen, typ, kmStand, treibstoff, alter, klimaanlage, sitzheitzung, verbrauch, schadstoffBelastung,
+                versicherung, grundtarif, verfuegbarkeit, tueren, leistung);
+        this.kofferraumGroesse = kofferraumGroesse;
+    }
+
+    public double getKofferraumGroesse() {
+        return kofferraumGroesse;
+    }
+
+    public void setKofferraumGroesse(double kofferraumGroesse) {
+        this.kofferraumGroesse = kofferraumGroesse;
+    }
+}
