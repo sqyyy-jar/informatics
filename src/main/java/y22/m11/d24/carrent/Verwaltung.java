d767ff6dd0b1e30ae74dd2f940a22b018a908f83
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-08 09:19:14 +0100

    Idk

--- src/main/java/y22/m11/d24/carrent/Verwaltung.java
+++ src/main/java/y22/m11/d24/carrent/Verwaltung.java
@@ -3,8 +3,8 @@ package y22.m11.d24.carrent;
 import informatics.Exercise;
 
 @Exercise("CarRent 24.11.2022")
-public class CarRent {
+public class Verwaltung {
     public static void run() {
-
+        System.out.println("Hello world!");
     }
 }
