af520eb5c3719d95625f309b51b28723434bf3e8
Felix <62748369+sqyyy-jar@users.noreply.github.com>
2022-12-08 21:30:49 +0100

    Add Notebook for CLI

--- src/main/java/informatics/Notebook.java
+++ src/main/java/informatics/Notebook.java
@@ -6,46 +6,22 @@ import com.github.sqyyy.jnb.Page;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Comparator;
 import java.util.List;
 
 public class Notebook {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InvocationTargetException, IllegalAccessException {
         if (args.length == 0) {
             showHelp();
             return;
         }
-        if (args.length != 1) {
-            System.out.println("The following pages were found:");
-            final List<Class<?>> classes = new ArrayList<>(JavaNotebooks.getPageClasses());
-            classes.sort(Comparator.comparing(Class::getName));
-            for (Class<?> aClass : classes) {
-                Page annotation = aClass.getAnnotation(Page.class);
-                System.out.printf("* [%s] %s\n", aClass.getName(), annotation.value());
-            }
-            return;
-        }
-        try {
-            final Class<?> clazz = Class.forName(args[0]);
-            if (!clazz.isAnnotationPresent(Page.class)) {
-                err("Class '" + args[0] + "' is not a page");
-                return;
-            }
-            final Page exercise = clazz.getAnnotation(Page.class);
-            System.out.println("=>> " + exercise.value());
-            final Method run = clazz.getDeclaredMethod("run");
-            run.invoke(null);
-        } catch (ClassNotFoundException e) {
-            err("Could not find class '" + args[0] + "'");
-        } catch (NoSuchMethodException e) {
-            err("Could not find method 'static void run()'");
-        } catch (InvocationTargetException e) {
-            System.out.println();
-            System.err.println("=".repeat(60));
-            e.getTargetException().printStackTrace();
-            err("Could not invoke method 'static void run()'");
-        } catch (IllegalAccessException e) {
-            err("Could not access something: " + e.getMessage());
+        final String subcommand = args[0];
+        switch (subcommand) {
+            case "pages", "p" -> pagesSubcommand(args);
+            case "entrypoints", "e" -> entrypointsSubcommand(args);
+            case "run", "r" -> runSubcommand(args);
+            default -> showHelp();
         }
     }
 
@@ -57,10 +33,75 @@ public class Notebook {
                 <notebook> [SUBCOMMAND]
                         
             SUBCOMMANDS:
-                pages, p        List all pages
-                entrypoints, e  List all entrypoints
-                run, r          Run an entrypoint
+                pages, p                List all pages
+                entrypoints, e          List all entrypoints
+                run, r [CLASS] [METHOD] Run an entrypoint
             """);
+    }
+
+    private static void pagesSubcommand(String[] args) {
+        if (args.length != 1) {
+            showHelp();
+            return;
+        }
+        System.out.println("The following pages were found:");
+        final ArrayList<Class<?>> pageClasses = new ArrayList<>(JavaNotebooks.getPageClasses());
+        pageClasses.sort(Comparator.comparing(Class::getName));
+        for (final Class<?> pageClass : pageClasses) {
+            final Page annotation = pageClass.getAnnotation(Page.class);
+            System.out.printf("* [%s] %s\n", pageClass.getName(), annotation.value());
+        }
+    }
+
+    private static void entrypointsSubcommand(String[] args) {
+        if (args.length != 1) {
+            showHelp();
+            return;
+        }
+        System.out.println("The following entrypoints were found:");
+        final ArrayList<Method> entrypointMethods = new ArrayList<>(JavaNotebooks.getEntrypointMethods());
+        entrypointMethods.sort(Comparator.comparing(it -> it.getDeclaringClass().getName()));
+        for (final Method entrypointMethod : entrypointMethods) {
+            System.out.printf("* [%s] %s\n", entrypointMethod.getDeclaringClass().getName(), entrypointMethod.getName());
+        }
+    }
+
+    private static void runSubcommand(String[] args) throws InvocationTargetException, IllegalAccessException {
+        if (args.length < 3) {
+            showHelp();
+            return;
+        }
+        final String entrypointClass = args[1];
+        final String entrypointMethod = args[2];
+        final ArrayList<Method> entrypointMethods = new ArrayList<>(JavaNotebooks.getEntrypointMethods());
+        final List<Method> entrypoints = entrypointMethods.stream()
+            .filter(it -> it.getDeclaringClass().getName().equals(entrypointClass) && it.getName().equals(entrypointMethod))
+            .toList();
+        final int size = entrypoints.size();
+        if (size == 0) {
+            System.out.println("The entrypoint could not be found");
+        } else if (size == 1) {
+            final Method entrypoint = entrypoints.get(0);
+            final int parameterCount = entrypoint.getParameterCount();
+            if (parameterCount == 0) {
+                if (args.length > 3) {
+                    System.out.println("The entrypoint does not support arguments");
+                    return;
+                }
+                entrypoint.invoke(null);
+            } else if (parameterCount == 1) {
+                if (args.length == 3) {
+                    entrypoint.invoke(null, (Object) new String[0]);
+                    return;
+                }
+                final String[] newArgs = Arrays.copyOfRange(args, 3, args.length);
+                entrypoint.invoke(null, (Object) newArgs);
+            } else {
+                System.out.println("The entrypoint is invalid");
+            }
+        } else {
+            System.out.println("There are several entrypoint matching this criteria");
+        }
     }
 
     private static void err(String message) {
