179ebf72982b4083f07d9eb243df37f99104763b
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-01-19 11:53:14 +0100

    Add quality documentation ah yes

--- src/main/java/y22/m11/d10/A4_35.java
+++ src/main/java/y22/m11/d10/A4_35.java
@@ -9,27 +9,38 @@ import java.util.Scanner;
 public class A4_35 {
     @Entrypoint
     public static void main() {
+        // erstellt einen scanner
         Scanner scanner = new Scanner(System.in);
+        // fragt den Tag ab
         System.out.print("Tag   : ");
         int t = scanner.nextInt();
+        // fragt den Monat ab
         System.out.print("Monat : ");
         int m = scanner.nextInt();
+        // speichert den Monat in einer anderen Variable, weil m verändert werden könnte
         int oldM = m;
+        // fragt das Jahr ab
         System.out.print("Jahr  : ");
         int j = scanner.nextInt();
+        // speichert das Jahr in einer anderen Variable, weil j verändert werden könnte
         int oldJ = j;
+        // falls m <=2 ist, erhöhe m um 10 und erniedrige j um 1, andernfalls erniedrige m um 2
         if (m <= 2) {
             m += 10;
             j--;
         } else {
             m -= 2;
         }
+        // berechnet die ganzzahligen Werte c = j / 100 und y = j % 100
         int c = j / 100;
         int y = j % 100;
+        // berechnet den ganzzahligen Wert h
         int h = (((26 * m - 2) / 10) + t + y + y / 4 + c / 4 - 2 * c) % 7;
+        // falls h < 0 sein sollte, erhöhe h um 7
         if (h < 0) {
             h += 7;
         }
+        // gibt das Ergebnis in der folgenden Form aus: Der 24.12.2001 ist ein Montag.
         System.out.printf("Der %d.%d.%d ist ein %s.\n", t, oldM, oldJ, switch (h) {
             case 0 -> "Sonntag";
             case 1 -> "Montag";
--- src/main/java/y22/m11/d10/A4_36.java
+++ src/main/java/y22/m11/d10/A4_36.java
@@ -9,26 +9,39 @@ import java.util.Scanner;
 public class A4_36 {
     @Entrypoint
     public static void main() {
+        // liest das Jahr ein
         System.out.print("Jahr: ");
         int j = new Scanner(System.in).nextInt();
         if (j < 1 || j > 8202) {
             System.out.println("Nur die Jahre von 1 bis 8202 sind gültig.");
             return;
         }
+        // berechnet a = j % 19
         int a = j % 19;
+        // berechnet b = j % 4
         int b = j % 4;
+        // berechnet c = j % 7
         int c = j % 7;
+        // bestimmt m
         int m = (8 * (j / 100) + 13) / 25 - 2;
+        // bestimmt s
         int s = j / 100 - j / 400 - 2;
+        // verändert m
         m = (15 + s - m) % 30;
+        // bestimmt n
         int n = (6 + s) % 7;
+        // bestimmt d
         int d = (m + 19 * a) % 30;
+        // falls d = 29 ist, setze d = 28
+        // andernfalls: falls d = 28 und a >= 11 ist, setze d = 27
         if (d == 29) {
             d = 28;
         } else if (d == 28 && a >= 11) {
             d = 27;
         }
+        // setze e
         int e = (2 * b + 4 * c + 6 * d + n) % 7;
+        // bestimme tag und monat
         int day = 21 + d + e + 1;
         int month;
         if (day > 31) {
@@ -37,6 +50,7 @@ public class A4_36 {
         } else {
             month = 3;
         }
+        // gibt das Datum des Ostersonntags für das eingelesenes Jahr aus
         System.out.printf("Der Ostersonntag ist im Jahr %d am %d. %s.\n", j, day, switch (month) {
             case 3 -> "März";
             case 4 -> "April";
