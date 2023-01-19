947a242813edc187a88306bae7b1b79ec7597d11
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-01-19 11:53:01 +0100

    Improve exercises

--- src/main/java/y23/m01/d19/Verwaltung.java
+++ src/main/java/y23/m01/d19/Verwaltung.java
@@ -8,8 +8,8 @@ public class Verwaltung {
     @Entrypoint
     public static void main() {
         Fachbereich mathe = new Fachbereich(0, "Mathe");
-        mathe.setVorlesung1(new Vorlesung("ABC1", "Algebra"));
-        mathe.setVorlesung2(new Vorlesung("ABC2", "Matrizen"));
+        mathe.setVorlesung1("ABC1", "Algebra");
+        mathe.setVorlesung2("ABC2", "Matrizen");
         System.out.println(mathe);
     }
 }
