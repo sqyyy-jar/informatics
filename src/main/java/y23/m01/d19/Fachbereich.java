dc9b05d1cbfd465d9724a58c5e757b8925e859b7
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-01-23 12:59:48 +0100

    Add exercises for 23rd January 2023

--- src/main/java/y23/m01/d19/Fachbereich.java
+++ src/main/java/y23/m01/d19/Fachbereich.java
@@ -3,16 +3,18 @@ package y23.m01.d19;
 public class Fachbereich {
     private int nummer;
     private String name;
+    private int vorlesungAnz;
     private Vorlesung vorlesung1;
     private Vorlesung vorlesung2;
     private Vorlesung vorlesung3;
+    private Vorlesung vorlesung4;
 
     public Fachbereich(int nummer, String name) {
         this.nummer = nummer;
         this.name = name;
+        this.vorlesungAnz = 2;
         this.vorlesung1 = new Vorlesung("n/a", "n/a");
         this.vorlesung2 = new Vorlesung("n/a", "n/a");
-        this.vorlesung3 = new Vorlesung("n/a", "n/a");
     }
 
     public int getNummer() {
@@ -35,34 +37,47 @@ public class Fachbereich {
         return vorlesung1;
     }
 
-    public void setVorlesung1(String nummer, String titel) {
-        this.vorlesung1 = new Vorlesung(nummer, titel);
+    public void setVorlesung1(Vorlesung vorlesung1) {
+        this.vorlesung1 = vorlesung1;
     }
 
     public Vorlesung getVorlesung2() {
         return vorlesung2;
     }
 
-    public void setVorlesung2(String nummer, String titel) {
-        this.vorlesung2 = new Vorlesung(nummer, titel);
+    public void setVorlesung2(Vorlesung vorlesung2) {
+        this.vorlesung2 = vorlesung2;
     }
 
     public Vorlesung getVorlesung3() {
         return vorlesung3;
     }
 
-    public void setVorlesung3(String nummer, String titel) {
-        this.vorlesung3 = new Vorlesung(nummer, titel);
+    public void setVorlesung3(Vorlesung vorlesung3) {
+        this.vorlesung3 = vorlesung3;
+    }
+
+    public Vorlesung getVorlesung4() {
+        return vorlesung4;
+    }
+
+    public void setVorlesung4(Vorlesung vorlesung4) {
+        this.vorlesung4 = vorlesung4;
+    }
+
+    public void fuegeVerlesungHinzu(Vorlesung vorlesung) {
+        switch (vorlesungAnz) {
+            case 0 -> vorlesung1 = vorlesung;
+            case 1 -> vorlesung2 = vorlesung;
+            case 2 -> vorlesung3 = vorlesung;
+            case 3 -> vorlesung4 = vorlesung;
+        }
+        vorlesungAnz++;
     }
 
     @Override
     public String toString() {
-        return "Fachbereich{" +
-               "nummer=" + nummer +
-               ", name='" + name + '\'' +
-               ", vorlesung1=" + vorlesung1 +
-               ", vorlesung2=" + vorlesung2 +
-               ", vorlesung3=" + vorlesung3 +
-               '}';
+        return "Fachbereich{" + "nummer=" + nummer + ", name='" + name + "', vorlesungAnz=" + vorlesungAnz + ", vorlesung1=" +
+               vorlesung1 + ", vorlesung2=" + vorlesung2 + ", vorlesung3=" + vorlesung3 + ", vorlesung4=" + vorlesung4 + '}';
     }
 }
