6fabe4fe86155f0cab8f9339492cd1e3396297b5
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-02-08 16:08:09 +0100

    Add exercises for 6 February 2023

--- src/main/java/y23/m01/d19/UniVerwaltung.java
+++ src/main/java/y23/m01/d19/UniVerwaltung.java
@@ -5,26 +5,28 @@ import com.github.sqyyy.jnb.Page;
 
 @Page("19.01.23")
 public class UniVerwaltung {
-    private static int fachbereichAnzahl = 0;
-    private static Fachbereich fachbereich1;
-    private static Fachbereich fachbereich2;
+    private int index;
+    private Fachbereich[] fachbereiche;
+
+    public UniVerwaltung() {
+        this.index = 0;
+        this.fachbereiche = new Fachbereich[100];
+    }
 
     @Entrypoint
     public static void main() {
+        UniVerwaltung verwaltung = new UniVerwaltung();
         Fachbereich mathe = new Fachbereich(0, "Mathe");
         mathe.fuegeVerlesungHinzu(new Vorlesung("ABC1", "Algebra"));
         mathe.fuegeVerlesungHinzu(new Vorlesung("ABC2", "Matrizen"));
         Fachbereich informatik = new Fachbereich(1, "Informatik");
         mathe.fuegeVerlesungHinzu(new Vorlesung("INF1", "Java"));
         mathe.fuegeVerlesungHinzu(new Vorlesung("INF2", "SQL"));
-        fuegeFachbereichHinzu(mathe);
-        fuegeFachbereichHinzu(informatik);
+        verwaltung.fuegeFachbereichHinzu(mathe);
+        verwaltung.fuegeFachbereichHinzu(informatik);
     }
 
-    public static void fuegeFachbereichHinzu(Fachbereich fachbereich) {
-        switch (fachbereichAnzahl++) {
-            case 0 -> fachbereich1 = fachbereich;
-            case 1 -> fachbereich2 = fachbereich;
-        }
+    public void fuegeFachbereichHinzu(Fachbereich fachbereich) {
+        fachbereiche[index++] = fachbereich;
     }
 }
