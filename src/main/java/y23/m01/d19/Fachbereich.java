0995a10f167a5d0eb7aaeb29ab72c9d80588a4ca
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-02-08 16:08:09 +0100

    Add exercises for 6 February 2023

--- src/main/java/y23/m01/d19/Fachbereich.java
+++ src/main/java/y23/m01/d19/Fachbereich.java
@@ -1,20 +1,18 @@
 package y23.m01.d19;
 
+import java.util.Arrays;
+
 public class Fachbereich {
     private int nummer;
     private String name;
-    private int vorlesungAnz;
-    private Vorlesung vorlesung1;
-    private Vorlesung vorlesung2;
-    private Vorlesung vorlesung3;
-    private Vorlesung vorlesung4;
+    private int index;
+    private Vorlesung[] vorlesungen;
 
     public Fachbereich(int nummer, String name) {
         this.nummer = nummer;
         this.name = name;
-        this.vorlesungAnz = 2;
-        this.vorlesung1 = new Vorlesung("n/a", "n/a");
-        this.vorlesung2 = new Vorlesung("n/a", "n/a");
+        this.index = 0;
+        this.vorlesungen = new Vorlesung[100];
     }
 
     public int getNummer() {
@@ -33,51 +31,20 @@ public class Fachbereich {
         this.name = name;
     }
 
-    public Vorlesung getVorlesung1() {
-        return vorlesung1;
+    public Vorlesung getVorlesung(int index) {
+        return vorlesungen[index];
     }
 
-    public void setVorlesung1(Vorlesung vorlesung1) {
-        this.vorlesung1 = vorlesung1;
-    }
-
-    public Vorlesung getVorlesung2() {
-        return vorlesung2;
-    }
-
-    public void setVorlesung2(Vorlesung vorlesung2) {
-        this.vorlesung2 = vorlesung2;
-    }
-
-    public Vorlesung getVorlesung3() {
-        return vorlesung3;
-    }
-
-    public void setVorlesung3(Vorlesung vorlesung3) {
-        this.vorlesung3 = vorlesung3;
-    }
-
-    public Vorlesung getVorlesung4() {
-        return vorlesung4;
-    }
-
-    public void setVorlesung4(Vorlesung vorlesung4) {
-        this.vorlesung4 = vorlesung4;
+    public void setVorlesung(int index, Vorlesung vorlesung) {
+        this.vorlesungen[index] = vorlesung;
     }
 
     public void fuegeVerlesungHinzu(Vorlesung vorlesung) {
-        switch (vorlesungAnz) {
-            case 0 -> vorlesung1 = vorlesung;
-            case 1 -> vorlesung2 = vorlesung;
-            case 2 -> vorlesung3 = vorlesung;
-            case 3 -> vorlesung4 = vorlesung;
-        }
-        vorlesungAnz++;
+        vorlesungen[index++] = vorlesung;
     }
 
     @Override
     public String toString() {
-        return "Fachbereich{" + "nummer=" + nummer + ", name='" + name + "', vorlesungAnz=" + vorlesungAnz + ", vorlesung1=" +
-               vorlesung1 + ", vorlesung2=" + vorlesung2 + ", vorlesung3=" + vorlesung3 + ", vorlesung4=" + vorlesung4 + '}';
+        return "Fachbereich{" + "nummer=" + nummer + ", name='" + name + "', vorlesungen=" + Arrays.toString(vorlesungen) + '}';
     }
 }
