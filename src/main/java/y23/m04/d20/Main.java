3b5c16dee032f4cf44905f03d455ec2b5678fb06
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-04-20 22:09:57 +0200

    Add testing swing UI

---
+++ src/main/java/y23/m04/d20/Main.java
@@ -0,0 +1,67 @@
+package y23.m04.d20;
+
+import com.github.sqyyy.jnb.Entrypoint;
+import com.github.sqyyy.jnb.Page;
+
+import javax.swing.*;
+import java.awt.*;
+
+@Page("23/04/20")
+public class Main {
+    private static final String METAL_THEME = "javax.swing.plaf.metal.MetalLookAndFeel";
+    private static final String NIMBUS_THEME = "javax.swing.plaf.nimbus.NimbusLookAndFeel";
+    private static final String MOTIF_THEME = "com.sun.java.swing.plaf.motif.MotifLookAndFeel";
+    private static final String GTK_THEME = "com.sun.java.swing.plaf.gtk.GTKLookAndFeel";
+    private static final int WIDTH = 1600;
+    private static final int HEIGHT = 900;
+    private static final DefaultListModel<String> NAME_LINES = new DefaultListModel<>();
+
+    @Entrypoint
+    public static void main(String[] main)
+        throws InterruptedException, UnsupportedLookAndFeelException, ClassNotFoundException, InstantiationException,
+        IllegalAccessException {
+        UIManager.setLookAndFeel(GTK_THEME);
+        NAME_LINES.addElement("Hello");
+        var frame = new JFrame("InfoCamp");
+        frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+        frame.setSize(WIDTH, HEIGHT);
+        frame.setLocation(1920 / 2 - WIDTH / 2, 1080 / 2 - HEIGHT / 2);
+        frame.setResizable(false);
+        var tabs = new JTabbedPane();
+        tabs.setFocusable(true);
+        var nameList = createNameListPanel();
+        tabs.addTab("Namensliste", nameList.panel());
+        frame.add(tabs);
+        frame.setVisible(true);
+        for (int i = 0; i < 100; i++) {
+            synchronized (NAME_LINES) {
+                NAME_LINES.addElement(String.valueOf(i));
+            }
+            Thread.sleep(50);
+        }
+    }
+
+    private static NameList createNameListPanel() {
+        var panel = new JPanel();
+        panel.setLayout(null);
+//        var refreshButton = new JButton("Neu laden");
+//        refreshButton.setBounds(20, 5, 100, 20);
+        var list = new JList<String>();
+        list.setModel(NAME_LINES);
+//        refreshButton.addActionListener(e -> {
+//            list.updateUI();
+//        });
+        var innerScrollPanel = new JPanel();
+        innerScrollPanel.setLayout(new BorderLayout());
+        innerScrollPanel.add(list);
+        var scrollPane = new JScrollPane(innerScrollPanel);
+        scrollPane.setBounds(5, 5, WIDTH - 10, HEIGHT - 70);
+        scrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+        scrollPane.getVerticalScrollBar()
+            .setUnitIncrement(16);
+        panel.setFocusable(true);
+//        panel.add(refreshButton);
+        panel.add(scrollPane);
+        return new NameList(panel, list);
+    }
+}
