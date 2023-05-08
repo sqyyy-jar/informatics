02a4b848db15ab3d226bb19123cc252ecca69270
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2023-05-08 11:53:28 +0200

    Update Teilnehmer class

--- src/main/java/y23/m05/d08/Teilnehmer.java
+++ src/main/java/y23/m05/d08/Teilnehmer.java
@@ -3,10 +3,12 @@ package y23.m05.d08;
 public class Teilnehmer {
     private String name;
     private String vorname;
+    private boolean essenBezahlt;
 
-    public Teilnehmer(String name, String vorname) {
+    public Teilnehmer(String name, String vorname, boolean essenBezahlt) {
         this.name = name;
         this.vorname = vorname;
+        this.essenBezahlt = essenBezahlt;
     }
 
     public String getName() {
@@ -15,5 +17,9 @@ public class Teilnehmer {
 
     public String getVorname() {
         return vorname;
+    }
+
+    public boolean hatEssenBezahlt() {
+        return essenBezahlt;
     }
 }
