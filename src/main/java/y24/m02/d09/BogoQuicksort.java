5b1a87f0c89a8113aa3deca1abffca85211e70d8
sqyyy-jar <62748369+sqyyy-jar@users.noreply.github.com>
2024-03-08 11:43:29 +0100

    feat: update stuff

--- src/main/java/y24/m02/d09/BogoQuicksort.java
+++ src/main/java/y24/m02/d09/BogoQuicksort.java
@@ -1,22 +1,27 @@
 package y24.m02.d09;
 
-import java.util.Arrays;
-import java.util.Collections;
 import java.util.Random;
 
 public class BogoQuicksort {
+    private final Pivot pivot;
     private final Random random;
 
-    public BogoQuicksort() {
+    public BogoQuicksort(Pivot pivot) {
+        this.pivot = pivot;
         this.random = new Random();
     }
 
-    public int partition(ObservedArray array, int left, int right) {
-        int pivotIndex = random.nextInt(left, right + 1);
-        swap(array, pivotIndex, right);
+    public int partition2(ArrayStats array, int left, int right) {
+        switch (this.pivot) {
+            case LEFT -> swap(array, left, right);
+            case MIDDLE -> swap(array, left + (right - left) / 2, right);
+            case RIGHT -> {}
+            case RANDOM -> swap(array, random.nextInt(left, right + 1), right);
+        }
         int pivot = array.get(right);
         int i = left - 1;
         for (int j = left; j <= right - 1; j++) {
+            array.incComps();
             if (array.get(j) < pivot) {
                 i++;
                 swap(array, i, j);
@@ -26,76 +31,61 @@ public class BogoQuicksort {
         return i + 1;
     }
 
-    public int partitionLeft(ObservedArray array, int left, int right) {
-        swap(array, left, right);
+    public int partition(ArrayStats array, int left, int right) {
+        switch (this.pivot) {
+            case LEFT -> swap(array, left, right);
+            case MIDDLE -> swap(array, left + (right - left) / 2, right);
+            case RIGHT -> {}
+            case RANDOM -> swap(array, random.nextInt(left, right + 1), right);
+        }
         int pivot = array.get(right);
-        int i = left - 1;
+        int i = left;
         for (int j = left; j <= right - 1; j++) {
+            array.incComps();
             if (array.get(j) < pivot) {
-                i++;
                 swap(array, i, j);
+                i++;
             }
         }
-        swap(array, i + 1, right);
-        return i + 1;
+        swap(array, i, right);
+        return i;
     }
 
-    public int partitionMid(ObservedArray array, int left, int right) {
-        swap(array, left + (right - left) / 2, right);
-        int pivot = array.get(right);
-        int i = left - 1;
-        for (int j = left; j <= right - 1; j++) {
-            if (array.get(j) < pivot) {
-                i++;
-                swap(array, i, j);
-            }
-        }
-        swap(array, i + 1, right);
-        return i + 1;
-    }
-
-    public int partitionRight(ObservedArray array, int left, int right) {
-        int pivot = array.get(right);
-        int i = left - 1;
-        for (int j = left; j <= right - 1; j++) {
-            if (array.get(j) < pivot) {
-                i++;
-                swap(array, i, j);
-            }
-        }
-        swap(array, i + 1, right);
-        return i + 1;
-    }
-
-    public void quickSort(ObservedArray array, int left, int right) {
+    public void quickSort(ArrayStats array, int left, int right) {
         if (right <= left) {
             return;
         }
-        int partitionIndex = partitionMid(array, left, right);
+        int partitionIndex = partition(array, left, right);
         quickSort(array, left, partitionIndex - 1);
         quickSort(array, partitionIndex + 1, right);
     }
 
-    public void swap(ObservedArray array, int i, int j) {
+    public void swap(ArrayStats array, int i, int j) {
         int tmp = array.get(i);
         array.set(i, array.get(j));
         array.set(j, tmp);
     }
 
-    public static ObservedArray newSorted(int length) {
+    public static ArrayStats newSorted(int length) {
         int[] array = new int[length];
         for (int i = 0; i < length; i++) {
             array[i] = 10 - i;
         }
-        return new ObservedArray(array);
+        return new ArrayStats(array);
     }
 
     public static void main(String[] args) {
-        BogoQuicksort bogoQs = new BogoQuicksort();
-        var array = newSorted(10);
-        System.out.println(array);
-        bogoQs.quickSort(array, 0, array.len() - 1);
-        System.out.println(array);
-        System.out.println(array.getReads() + "/" + array.getWrites());
+        Tests.testAll();
+//        BogoQuicksort bogoQs = new BogoQuicksort(Pivot.MIDDLE);
+////        var array = newSorted(10);
+//        var array = new ArrayStats(9, 8, 7, 6, 5, 4, 3, 2, 1);
+//        System.out.println(array);
+//        bogoQs.quickSort(array, 0, array.len() - 1);
+//        System.out.println(array);
+//        System.out.println(array.getReads() + "/" + array.getWrites() + "/" + array.getComps());
+    }
+
+    public enum Pivot {
+        LEFT, MIDDLE, RIGHT, RANDOM;
     }
 }
